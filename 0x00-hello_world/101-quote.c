b03e90d2e16bea016abbefb970d98ae7d64b0c99
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-18 12:38:21 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - prints " .... " followed by newline 
- * to stantard error
- * Return: Always 1
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to stantard error.
+ * Return: Always 1.
  */
 
 int main(void)
+
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
