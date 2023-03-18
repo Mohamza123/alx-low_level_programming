7afb17189fb4e49c5b490006fd3f943c363ae1ad
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-18 14:40:44 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Test if the numper is positive or negative
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive", n);
+	else if (n < 0)
+		printf("%i is negative", n);
+	else
+		printf("%i is zero", n);
+
+	return (0);
+}
