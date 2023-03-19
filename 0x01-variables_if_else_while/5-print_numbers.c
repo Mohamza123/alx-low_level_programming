2397fd2be2e3ffea458d321b78be4b75e887385c
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-19 05:11:05 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 0
+ * Return: Always(0)
+ *
+ */
+
+int main(void)
+
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf(x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
