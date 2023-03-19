e446eb6d6cd02c9bfecba1d58bc3d7cc89e43b4e
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-19 06:43:04 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,17 @@ int main(void)
 
 	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i + '1'; j <= '9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
 			if (i != j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i != '8' && j != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				if (i == '8' && j == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+
 			}
 		}
 	}
