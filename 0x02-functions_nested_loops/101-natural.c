f047ec01d3db2407dd24c06cc81ba41aaf2a8e07
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-21 14:23:51 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of multiplies of 3 and 5
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 1023; i > 2; i--)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%i\n", sum);
+}
