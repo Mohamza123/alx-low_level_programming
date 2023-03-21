8c1a1848e25cf6e517948c0cf9902d8108d54ac7
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-21 06:22:37 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return: Null
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		print_aplabet();
+	}
+}
