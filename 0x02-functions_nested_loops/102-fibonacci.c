1ea1ccde60a3012bc5721df8b580794c13a92881
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-21 14:35:46 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - to clculate fibonacci for the first 50;
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int f, s, i, th;
+
+	f = 1;
+	printf("1, ");
+	s = 2;
+	printf("2, ");
+	for (int i = 0; i <= 48; i++)
+	{
+		th = f + s;
+		f = s;
+		s = th;
+		printf("%i");
+		if (i < 48)
+		{
+			printf(", ");
+		}
+
+	}
+	printf("\n");
+	return (0);
+}
