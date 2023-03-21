d355d1bf6863b773e621b502d11843c789c6f033
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-21 07:41:53 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - from its name
+ * @c: the number
+ * Return: the last digit
+ *
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
