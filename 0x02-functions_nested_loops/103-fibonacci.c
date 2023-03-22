4cd671e84f02e828aafb471ffabe43490964a21b
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-22 08:15:23 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,19 +11,20 @@ int main(void)
 
 	f = 1;
 	s = 2;
+	sum = 2;
 	for (i = 0; i < 48; i++)
 	{
-		th = f + s;
-		if (th < 4000000)
+		if (f + s > 4000000)
 		{
-			if (th % 2 == 0)
-			{
-				sum += th;
-			}
-			f = s;
-			s = th;
+			break;
 		}
-
+		th = f + s;
+		if (th % 2 == 0)
+		{
+			sum += th;
+		}
+		f = s;
+		s = th;
 	}
 	printf("%li\n", sum);
 	return (0);
