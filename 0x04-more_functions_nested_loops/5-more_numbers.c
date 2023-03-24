c7807f208dda6b9335ffc69cecd751e03ebf7228
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-24 16:56:01 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 9
+ * Return: Null
+ *
+ */
+void more_numbers(void)
+{
+	int i, f, s;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+		{
+			s = i / 10;
+			_putchar(s + '0');
+		}
+		f = i % 10;
+		_putchar(f + '0');
+	}
+	_putchar('\n');
+}
