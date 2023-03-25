81f21b7f223cf2ff030c2822010b7ef02bb72e23
Mohamza123 <m.hamza.whedy@gmail.com>
2023-03-24 17:13:22 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a strait line
+ * @n: number of times
+ * Return: Null
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_')
+		}
+	}
+	_putchar('\n');
+}
