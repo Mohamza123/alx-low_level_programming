80d9d286a884ffbbb92052c576e832a70659a2fb
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-12 12:02:10 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,13 @@ int _atoi(char *s)
 {
 	int i;
 	int sign = 1;
-	int num = 0;
+	unsigned int num = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 			num = (num * 10) + (s[i] - '0');
-		else if (s[i] == '-')
+		else if (s[i] == '-' && (s[i + 1] >= '0' && (s[i + 1] <= '9')
 			sign *= -1;
 	}
 	return (num * sign);
