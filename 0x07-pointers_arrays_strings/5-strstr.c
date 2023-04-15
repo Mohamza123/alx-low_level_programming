8bc821164a839528d283c960e30a08f4a946a4d4
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-15 11:37:23 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strstr - get the length of a prefix substring
+ * @haystack: the string
+ * @needle: to be scaned
+ * Return: haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (haystack[i]);
+		}
+	}
+	return (0);
+}
