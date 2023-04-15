a90ae9c4a1729b43639350c4d951eabfa6c8f668
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-14 19:01:04 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ * @s1: first one
+ * @s2: second one
+ * Return: integer contains 0 if mathced
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	n = s1[0] - s2[0];
+	return (n);
+}
