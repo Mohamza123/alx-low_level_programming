ba24fe554b579c236c7e4aa586fd61d0d8c246d6
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-19 07:11:04 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - map an array through functiont pointer
+ * @array: the arr
+ * @size: it's size
+ * @action: the func pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+	if (array && size && action)
+	{
+		while (array <= end)
+			action(*array++);
+	}
+}
