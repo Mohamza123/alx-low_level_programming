f0995121b9eaab96c6bdfb70d10abccdd4e4f412
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-28 13:23:52 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mul two nums
+ * @argc: arg count
+ * @argv: arg values
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
