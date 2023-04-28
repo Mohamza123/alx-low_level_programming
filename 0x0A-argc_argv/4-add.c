a321544d7d511fab29adec13c7b8f897689f20e7
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-28 13:45:54 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * isnum - check if the string is a num or not
+ * @str: the string
+ * Return: 0 if it's a num otherwise 1
+ */
+int isnum(char *str)
+{
+	int i = 0;
+
+	while (*str)
+	{
+		if (!isdigit(str[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * main - add two nums
+ * @argc: arg count
+ * @argv: arg values
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		if (isnum(argv[i]) == 1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
