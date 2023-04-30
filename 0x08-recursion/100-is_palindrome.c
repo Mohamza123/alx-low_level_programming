e7954089fd0e98262e297e08a089f52cf6490698
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-29 17:00:05 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,14 +8,24 @@
  */
 int is_p(int f, int l, char *s)
 {
-	if (s[f] != s[l])
+	if (s[f] != s[l - 1])
 		return (0);
-	else if (f == l || f + 1 == l)
+	else if (f == l - 1 || f + 1 == l - 1)
 		return (1);
 	else
 		return (is_p(f + 1, l - 1, s));
 }
-
+/**
+ * len - get the length of a string
+ * @s: the string
+ * Return: the length
+ */
+int len(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + len(s + 1));
+}
 /**
  * is_palindrome - check a string
  * @s: the string
@@ -23,10 +33,8 @@ int is_p(int f, int l, char *s)
  */
 int is_palindrome(char *s)
 {
-	int l = 0;
+	int l = len(s);
 
-	while (s[l])
-		l++;
 	if (l <= 1)
 		return (1);
 	else
