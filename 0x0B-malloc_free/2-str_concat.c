f4a947cd8c3b8fecfc58256a1be6c534f936665e
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-30 04:29:46 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int l1 = 0, l2 = 0, r;
 
-	if (*s1 == NULL && *s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	while (s1[l1])
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 		l2++;
 
 	s1 = (char *)realloc(s1, sizeof(char) * (l1 + l2 + 1));
-	for (r = 0; r < l2 + 1; r++, l1++)
+	for (r = 0; r < (l2 + 1); r++, l1++)
 	{
 			s1[l1 + 1] = s2[r];
 	}
