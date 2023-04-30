8bcf0c66e8c157ea6cd26a918ec976805c70b10b
Mohamza123 <m.hamza.whedy@gmail.com>
2023-04-30 03:51:08 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup - copy a string to ne loccation
+ * @str: the string to copy
+ * Return: a pointer to the new string or NULL
+ */
+char *_strdup(char *str)
+{
+	unsigned int l = 0;
+	char *str2;
+
+	while (str[l])
+		l++;
+	if (str == NULL)
+		return (NULL);
+
+	*str2 = malloc(sizeof(char) * (l + 1));
+	while (str)
+	{
+		*str2 = *str;
+		str2++;
+		str++;
+	}
+	return (str2);
+}
