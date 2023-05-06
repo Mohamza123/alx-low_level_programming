a43cbf068433929be6a7f9167e65c3bfabc0bf8d
Mohamza123 <m.hamza.whedy@gmail.com>
2023-05-06 10:41:45 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - make the adress of an int 98
- * @n: the adress of an int
- * Return: Always null
+ * reset_to_98 - change a value to 98
+ * @n: a pointer to an int
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
