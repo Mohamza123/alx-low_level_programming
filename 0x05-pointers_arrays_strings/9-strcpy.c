80d42af9a6653521932837413075d4e70068277f
Mohamza123 <m.hamza.whedy@gmail.com>
2023-05-06 14:21:51 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy a atring to new one
+ * @src: the str
+ * @dest: the destination
+ * Return: dest
+ */
+char *_strcpy(char *src, char *dest)
+{
+	int i, len = 0;
+
+	while (src[len])
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[len] = '\0';
+}
