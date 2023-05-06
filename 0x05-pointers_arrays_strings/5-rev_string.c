3171e608e4f7c20216b80b8f356f56cb2c0b125a
Mohamza123 <m.hamza.whedy@gmail.com>
2023-05-06 13:15:45 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
 void rev_string(char *s)
 {
 	int i, len = 0;
-	char tmp;
+	char temp;
 
 	while (s[len])
 		len++;
 
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = s[i];
+		temp = s[i];
 		s[i] = s[len];
 		s[len] = s[i];
 		len--;
