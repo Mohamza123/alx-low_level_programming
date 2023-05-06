d61a22e868dde11dc25756ec78ac8ff9df072207
Mohamza123 <m.hamza.whedy@gmail.com>
2023-05-06 12:50:43 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,16 @@ void print_rev(char *s)
 	int len;
 
 	len = 0;
-	while (s[len])
-		len++;
-	while (len > 0)
+	while (*s)
 	{
-		_putchar(s[len]);
+		len++;
+		s++;
+	}
+	while (len >= 0)
+	{
+		_putchar(*s);
 		len--;
+		s--;
 	}
 	_putchar('\n');
 }
